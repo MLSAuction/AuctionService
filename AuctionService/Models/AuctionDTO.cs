06bb0d26444155009e65fc3101fccdbec92a6304
ChaoticFS <Magnus.overgaard1711@gmail.com>
2023-12-14 14:06:22 +0100

    updated dto with new field, needs to be implemented in code

--- AuctionService/Models/AuctionDTO.cs
+++ AuctionService/Models/AuctionDTO.cs
@@ -15,5 +15,6 @@ namespace AuctionService.Models
         public int BuyNowPrice {get;set;}
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
