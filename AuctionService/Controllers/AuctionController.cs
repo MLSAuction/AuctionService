f517a35dc5631102812cc47b19e948059a3b9e3f
ChaoticFS <Magnus.overgaard1711@gmail.com>
2023-12-14 10:09:20 +0100

    inverted auth on getting auctions so we can use it for front page type action

--- AuctionService/Controllers/AuctionController.cs
+++ AuctionService/Controllers/AuctionController.cs
@@ -24,7 +24,7 @@ namespace AuctionService.Controllers
         /// Get all auctions
         /// </summary>
         /// <returns></returns>
-        [Authorize]
+        [AllowAnonymous]
         [HttpGet("getAll")]
         public IActionResult GetAllAuctions()
         {
